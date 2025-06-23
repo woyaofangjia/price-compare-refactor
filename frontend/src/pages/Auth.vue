706eec3bb56e8ac03ed237e6669a19ca3b84da0f
vvan汪汪队大队长 <whu13611614482@163.com>
2025-06-23 15:53:17 +0800

    feat: 添加比价页面路由及组件

---
+++ frontend/src/pages/Auth.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="page-content">
+    <div class="container">
+      <h2 class="section-title">用户认证模块</h2>
+      <p>这里是用户认证模块。</p>
+    </div>
+  </section>
+</template> 
